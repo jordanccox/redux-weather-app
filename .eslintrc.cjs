4070267839f526fc0527f4a6460dc641c6f56c4e
Jordan Cox <jordancox747@outlook.com>
2023-10-22 06:44:27 -0600

    eslint setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "wesbos"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -16,5 +13,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "no-console": 2,
   },
 }
